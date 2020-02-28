11ece4518bcdb5cd0240d7b73914ef7f8562a96e
Phavuk <43843255+Phavuk@users.noreply.github.com>
2020-02-28 11:13:38 +0100

    commit

---
+++ hibernate/src/main/java/hibernate/DeleteStudentDemo.java
@@ -0,0 +1,34 @@
+package hibernate;
+
+import hibernate.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteStudentDemo {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            int studentId = 1;
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            // delete student through query
+            session.createQuery("delete from Student where id='2'").executeUpdate();
+
+            // commit the update operation
+            session.getTransaction().commit();
+
+
+        } finally {
+            factory.close();
+        }
+    }
+}
---
+++ hibernate2/src/main/java/CreateDemo.java
@@ -0,0 +1,40 @@
+import entity.Instructor;
+import entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateDemo {
+
+    public static void main(String[] args) {
+
+    SessionFactory factory = new Configuration()
+            .configure("hibernate.cfg.xml")
+            .addAnnotatedClass(Instructor.class)
+            .addAnnotatedClass(InstructorDetail.class)
+            .buildSessionFactory();
+
+    Session session = factory.getCurrentSession();
+
+        try {
+
+        Instructor tempInstructor = new Instructor("johnny","spider","john@spider.com");
+
+        InstructorDetail tempInstructorDetail = new InstructorDetail("youtube.com/test","test");
+
+        tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+        session.beginTransaction();
+
+        session.save(tempInstructor);
+
+        System.out.println("Commit transaction");
+        session.getTransaction().commit();
+
+        System.out.println("Done");
+
+        } finally {
+            factory.close();
+        }
+    }
+}
